67a2770b86cdc5e1b9dc3d46ec9158dbd1de2bf1
Dan Cogswell <cogswell@mit.edu>
2021-02-22 15:01:36 -0500

    Download and process additional hospitalization ensemble forecasts only available at the CDC website.

--- forecasts/COVIDhubensemble.py
+++ forecasts/COVIDhubensemble.py
@@ -1,6 +1,10 @@
 import os.path as osp
 from os import walk
 import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+
+import utils
 
 dir = 'forecasts/covid19-forecast-hub/data-processed'
 
@@ -10,19 +14,20 @@ def list_models():
     return(dirnames)
 
 
-#Returns a dictionary of models and weights to create the ensemble
+# Returns a dictionary of models and weights to create the ensemble
 def ensemble_model_weights_cumDeaths(date, location="US"):
     datestr = pd.to_datetime(date).strftime('%Y-%m-%d')
-    file= osp.join(dir,'../ensemble-metadata/',datestr+'-cum_death-model-weights.csv')
-    weights=pd.read_csv(file)
+    file = osp.join(dir, '../ensemble-metadata/',
+                    datestr+'-cum_death-model-weights.csv')
+    weights = pd.read_csv(file)
 
-    #Filter for location and remove field
-    loc=weights[weights['locations']==location]
-    loc=loc.drop('locations',1)
+    # Filter for location and remove field
+    loc = weights[weights['locations'] == location]
+    loc = loc.drop('locations', 1)
 
     models = loc.keys()
     wts = loc[models].iloc[0]
-    return(dict(zip(models,wts)))
+    return(dict(zip(models, wts)))
 
 
 def list_dates(model="COVIDhub-ensemble"):
@@ -31,7 +36,7 @@ def list_dates(model="COVIDhub-ensemble"):
 
     for f in filenames:
         # Get the date from the first 10 characters of the filename
-        if "csv" in f:
+        if model+".csv" in f:
             dates.append(pd.to_datetime(f[:10]))
 
     dates.sort()
@@ -41,13 +46,14 @@ def list_dates(model="COVIDhub-ensemble"):
 def load(date, location="US", model="COVIDhub-ensemble"):
 
     # Load locations table
+    location_id = location
     loc_df = pd.read_csv(osp.join(dir, '../data-locations/locations.csv'))
     abbr = loc_df[loc_df['abbreviation'] == location]
     if len(abbr):
-        location = abbr['location'].iat[0]
+        location_id = abbr['location'].iat[0]
     else:
-        location = loc_df[loc_df['location_name']
-                          == location]['location'].iat[0]
+        location_id = loc_df[loc_df['location_name']
+                             == location]['location'].iat[0]
 
     # Load the data file
     file = pd.to_datetime(date).strftime('%Y-%m-%d') + '-' + model + '.csv'
@@ -57,7 +63,7 @@ def load(date, location="US", model="COVIDhub-ensemble"):
     })
 
     # Filter for location
-    data = data[data['location'] == location]
+    data = data[data['location'] == location_id]
 
     # Filter for point data
     data = data[data['type'] == 'point']
@@ -68,9 +74,15 @@ def load(date, location="US", model="COVIDhub-ensemble"):
 
     # #Define standard fields
     hospitalAdmissions_df = data[data['target'].str.contains('inc hosp')]
-    hospitalAdmissions_df = pd.DataFrame({
-        'date': hospitalAdmissions_df['target_end_date'],
-        'hospitalAdmissions': hospitalAdmissions_df['value']})
+
+    # Before 12/7/20, COVIDhub-ensemble hospitalizations are reported in a separate file downloaded from the CDC website
+    if date < datetime(2020, 12, 7):
+        hospitalAdmissions_df = load_CDC_ensemble_hospitalization(
+            date, location=location)
+    else:
+        hospitalAdmissions_df = pd.DataFrame({
+            'date': hospitalAdmissions_df['target_end_date'],
+            'hospitalAdmissions': hospitalAdmissions_df['value']})
 
     cumDeath_df = data[data['target'].str.contains('cum death')]
     cumDeath_df = pd.DataFrame({
@@ -81,3 +93,64 @@ def load(date, location="US", model="COVIDhub-ensemble"):
     merged = pd.merge(hospitalAdmissions_df, cumDeath_df,
                       on='date', sort=True, how='outer')
     return(merged)
+
+
+# This function loads hospitalization forecasts downloaded from the CDC website
+def load_CDC_ensemble_hospitalization(date, location="US", model="COVIDhub-ensemble"):
+
+    # Get the file name
+    file = ''
+    dateStr = pd.to_datetime(date).strftime('%Y-%m-%d')
+    _, _, filenames = next(walk(osp.join(dir, 'COVIDhub-ensemble')))
+    for f in filenames:
+        if 'hospitalizations-model-data' in f:
+            if dateStr in f:
+                file = f
+
+    if file == '':
+        return(pd.DataFrame({'date': [], 'hospitalAdmissions': []}))
+
+    data = pd.read_csv(osp.join(dir, model, file))
+
+    # Filter for model
+    if model != 'COVIDhub-ensemble':
+        data = data[data['model'] == model]
+
+    # Filter for location
+    if location == 'US':
+        location = 'National'
+    # If location is an abbreviation, convert to name
+    if location in utils.abbr_to_state.keys():
+        location = utils.abbr_to_state[location]
+    data = data[data['location_name'] == location]
+
+    # Convert dates to datetime
+    data['target_end_date'] = pd.to_datetime(data['target_end_date'])
+
+    # Compute the ensemble average if necessary
+    if model == 'COVIDhub-ensemble':
+        if 'Ensemble' in data['model'].unique():
+            data = data[data['model'] == 'Ensemble']
+        else:
+            # Calculate forcast for each model by summing all locations
+            COVIDhub_df = pd.DataFrame()
+            for m in data['model'].unique():
+
+                # Sum over all locations and append
+                model_df = data[data['model'] == m].groupby(
+                    'target_end_date', as_index=False).sum()
+                COVIDhub_df = COVIDhub_df.append(model_df)
+
+            # Calculate the median of all models
+            data = COVIDhub_df.groupby(
+                'target_end_date', as_index=False).median()
+
+    # Filter days ahead <= 28
+    mask = data['target_end_date'] - date <= timedelta(days=28)
+
+    # Define standard fields
+    new_df = pd.DataFrame({
+        'date': data['target_end_date'][mask],
+        'hospitalAdmissions': data['point'][mask]
+    })
+    return(new_df)
--- forecasts/update-covid19-forecast-hub.sh
+++ forecasts/update-covid19-forecast-hub.sh
@@ -8,3 +8,27 @@ then
 else
     git -C $git_dir pull
 fi
+
+
+#Get additional hospitalization forecasts only available at the CDC website
+storage_dir="covid19-forecast-hub/data-processed/COVIDhub-ensemble"
+URL="https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/forecasting-hospitalizations-previous.html"
+files=`curl -s $URL | egrep -o '\/[^ ]*(-all)?-hospitalizations-model-data.csv'`
+
+#Download data files
+for f in $files
+do
+  
+  #Check if filename does not contain "national"
+  if [[ ! "$f" == *"national"* ]]
+  then
+    
+    #Download file if it doesn't exist yet
+    fname=`echo $f | egrep -o '[0-9]{4}-[0-9]{2}-[0-9]{2}[^ ]*.csv'`
+    if [ ! -f $storage_dir/$fname ]
+    then
+      echo "Downloading https://www.cdc.gov/$f"
+      wget -P $storage_dir https://www.cdc.gov/$f
+    fi
+  fi
+done
